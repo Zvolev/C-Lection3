fe03147ede3720804fab9d8791e6dfa2f8369590
Evgenii <zvolev@mail.ru>
2022-10-11 16:07:12 +0300

    добавил задачу со строками

---
+++ Ext2/Program.cs
@@ -0,0 +1,17 @@
+﻿string text = " -  Я думаю, - сказал князь, улыбаясь, - что,"
+            + "ежели бы вас послали вместо нашего милого Винценгороде,"
+            + "вы бы взяли приступом согласие прусского короля";
+
+string Riplace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Riplace(text,' ', '$');
+System.Console.WriteLine(newText);
